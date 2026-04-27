c09bab0b688ec3ec9190e86155c8c9cd40f1023e
djandDK <ole.friis.nielsen@gmail.com>
2026-04-27 19:14:04 +0200

    Initial commit

--- Site/api/v1/inc/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("DB_HOST", "localhost");
-define("DB_PORT", "3306");
-define("DB_USERNAME", "<username>");
-define("DB_PASSWORD", "<password>");
-define("DB_DATABASE_NAME", "<databasename>");
-define("WarThunderAPI", "<warthunderapi>);
-?>
--- Site/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="py-3 my-4">
-	<ul class="nav justify-content-center border-bottom pb-3 mb-3" id="generalSiteStats">
-		<li class="px-3 text-body-secondary"></li>
-		<li class="px-3 text-body-secondary"></li>
-		<li class="px-3 text-body-secondary"></li>
-	</ul>
-	<p class="text-center text-body-secondary">Should probably put something here</p>
-</footer>
-<script>getGeneralSiteStats()</script>
